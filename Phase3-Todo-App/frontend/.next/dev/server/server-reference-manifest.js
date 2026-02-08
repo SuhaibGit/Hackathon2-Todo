8754ada4caa820a5391412264f06620e0e2c94b6
SuhaibGit <m.suhaib1906@gmail.com>
2026-02-08 13:31:02 +0500

    folder renamed

--- Phase3-Todo-App/frontend/.next/dev/server/server-reference-manifest.js
+++ Phase3-Todo-App/frontend/.next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qkvKHJo4FivyN4X+/6xJRIpQuvvwpzqzVyRrx5EP1ME=\"\n}"
